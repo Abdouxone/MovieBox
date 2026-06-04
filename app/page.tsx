77ae1eef687a57e55d1ca829e7aba8c52235787d
Abdou se <abdouselim.edu@gmail.com>
2026-06-04 09:04:29 +0100

    adding posters and movies name

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
 "use client";
 import HeroSection from "@/components/HeroSection";
+import { StarIcon } from "lucide-react";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
+export interface MovieData {
+  vote_average: number;
+  title: string;
+  poster_path: string;
+  overview: string;
+  release_date: string;
+}
+
 export default function Home() {
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState<MovieData[]>([]);
   const [loading, isLoading] = useState(true);
 
   useEffect(() => {
@@ -27,24 +36,35 @@ export default function Home() {
     <div>
       <HeroSection />
       <div className="flex flex-col mx-10 ">
-        <div className="space-y-5">
+        <div className="space-y-5 mb-7 ">
           <h1 className="font-bold text-4xl">Popular Movies right now!</h1>
-          <p className="text-lg">Explore new names today</p>
+          <p className="text-lg text-santas-gray">Explore new names today</p>
         </div>
-        <div className="grid grid-cols-5 gap-3 ">
+        <div className="grid grid-cols-5 gap-3  ">
           {movies.map((data, index) => (
             <div
-              className=" overflow-hidden rounded-lg shadow h-100 group relative"
+              className=" overflow-hidden flex items-center justify-center  rounded-lg shadow  group relative"
               key={index}
             >
               <div className="absolute z-10 inset-0 bg-linear-to-t from-woodsmoke/50  to-transparent "></div>
-              <div className="">
-                <Image
-                  src={`https://image.tmdb.org/t/p/w500/${data.poster_path}`}
-                  alt=""
-                  fill
-                  className="object-cover h-fit group-hover:scale-105 transition-all duration-300"
-                />
+              <Image
+                src={`https://image.tmdb.org/t/p/w500/${data.poster_path}`}
+                alt=""
+                height={400}
+                width={250}
+                className="object-cover w-full h-full group-hover:scale-105 transition-all duration-500"
+              />
+              <div className="absolute flex flex-col z-10 bg-woodsmoke shadow-lg text-left text-[#FFFFFF]  w-full h-15 right-0 bottom-0">
+                <span className="text-xl">{data.title}</span>
+                <span className="text-base">
+                  {data.release_date.split("-")[0]}
+                </span>
+              </div>
+              <div className="absolute flex  items-center space-x-2 px-2  justify-center rounded-2xl flex-row  top-2 right-2 bg-dark-black">
+                <span className="text-lg font-semibold">
+                  {data.vote_average.toFixed(2)}
+                </span>
+                <StarIcon size={20} color="yellow" />
               </div>
             </div>
           ))}
